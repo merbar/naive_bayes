905df2e551f8c04af17c8827d63ceef65f3a5ce8
Marcus Erbar <marcus.erbar@gmail.com>
2017-07-13 23:09:30 -0700

    accepts multiple feature vectors

--- classifier.py
+++ classifier.py
@@ -1,9 +1,16 @@
-
+import numpy as np
+import math
 
 class GNB(object):
 
 	def __init__(self):
 		self.possible_labels = ['left', 'keep', 'right']
+		# holds mean and std_dev for each class
+		self.class_summaries = {}
+
+	def calculateProbability(self, x, mean, stdev):
+		exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+		return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
 
 	def train(self, data, labels):
 		"""
@@ -11,18 +18,21 @@ class GNB(object):
 
 		INPUTS
 		data - array of N observations
-		  - Each observation is a tuple with 4 values: s, d, 
-		    s_dot and d_dot.
-		  - Example : [
-			  	[3.5, 0.1, 5.9, -0.02],
-			  	[8.0, -0.3, 3.0, 2.2],
-			  	...
-		  	]
+		  - Each observation is a tuple with x values
 
 		labels - array of N labels
 		  - Each label is one of "left", "keep", or "right".
 		"""
-		pass
+		data_cols_i = data.shape[-1]
+		for i in range(len(self.possible_labels)):
+			self.class_summaries[i] = []
+			for col in range(data_cols_i):
+				col_data = data[:,col]
+				label = self.possible_labels[i]
+				indeces = [x for x in range(len(labels)) if labels[x]==label]
+				mean = np.mean(col_data[indeces])
+				std_dev = np.std(col_data[indeces])
+				self.class_summaries[i].append((mean, std_dev))
 
 	def predict(self, observation):
 		"""
@@ -31,13 +41,26 @@ class GNB(object):
 
 		INPUTS
 
-		observation - a 4 tuple with s, d, s_dot, d_dot.
-		  - Example: [3.5, 0.1, 8.5, -0.2]
+		observation - a tuple of size x, same as used for training
 
 		OUTPUT
 
 		A label representing the best guess of the classifier. Can
 		be one of "left", "keep" or "right".
 		"""
-		# TODO - complete this
-		return self.possible_labels[1]
+		obs_cols_i = len(observation)
+		# get probability for each label
+		pred = [0.0 for i in range(len(self.possible_labels))]
+		for obs_i in range(obs_cols_i):
+			for label_i in range(len(self.possible_labels)):
+				# sum up probabilities for each data point
+				cur_observation = observation[obs_i]
+				mean = self.class_summaries[label_i][obs_i][0]
+				std_dev = self.class_summaries[label_i][obs_i][1]
+				prob = self.calculateProbability(cur_observation, mean, std_dev)
+				pred[label_i] += prob
+
+		# get max probability
+		max_v = max(pred)
+		max_i = pred.index(max_v)
+		return self.possible_labels[max_i]
--- prediction.py
+++ prediction.py
@@ -1,13 +1,18 @@
-#!/usr/bin/env python
-
 from classifier import GNB
+import numpy as np
 import json
 
 def main():
+	# TRAINING
 	gnb = GNB()
 	with open('train.json', 'r') as f:
    		j = json.load(f)
-	print(j.keys())
+	#print(j.keys())
+	'''
+   	X - array of N observations
+		  - Each observation is a tuple with 4 values: s, d, 
+		    s_dot and d_dot.
+	'''
 	X = j['states']
 	X = np.array(X)
 	Y = j['labels']
@@ -15,16 +20,22 @@ def main():
 	# d-values seem biased where 0 is the center of the left lane.
 	X[:,1] = X[:,1] + 2
 
+	# just feed delta d to classifier
+	#X = np.array([X[:,3]]).T
+	# feed d and delta_d into classifier
+	X = np.vstack((X[:,2], X[:,3])).T
 	gnb.train(X, Y)
 
-	with open('test.json', 'rb') as f:
+	# CLASSIFICATION
+	with open('test.json', 'r') as f:
 		j = json.load(f)
-
 	X = j['states']
 	X = np.array(X)
 	Y = j['labels']
 	Y = np.array(Y)
 	X[:,1] = X[:,1] + 2
+	#X = np.array([X[:,3]]).T
+	X = np.vstack((X[:,2], X[:,3])).T
 	score = 0
 	for coords, label in zip(X,Y):
 		predicted = gnb.predict(coords)
